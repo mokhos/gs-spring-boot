36b34217e41d437249053b873fbe7bca182cf875
mokhos <60837530+mokhos@users.noreply.github.com>
2021-06-28 13:51:42 +0200

    fix unknown error

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from -- UPDATED -- Spring Boot!";
+        return "Hello from UPDATED Spring Boot!";
     }   
 }
